ea9963d3c0192b4f384fa13ddaa8f549cf7b1895
Arbdoul <salisuabdullahi64@gmail.com>
2024-02-15 02:06:06 +0100

    display course lists

--- Apps/components/CourseItem.jsx
+++ Apps/components/CourseItem.jsx
@@ -12,13 +12,14 @@ const CourseItem = ({ course }) => {
         marginRight: 15,
         padding: 10,
         borderRadius: 10,
+        gap: 4,
       }}
     >
       <Image
         source={{ uri: course.banner.url }}
         style={{ width: 240, height: 130, borderRadius: 15 }}
       />
-      <View>
+      <View style={{ display: "flex", gap: 3 }}>
         <Text style={{ fontSize: 16, fontFamily: "outfit-bold" }}>
           {course?.name}
         </Text>
@@ -27,20 +28,48 @@ const CourseItem = ({ course }) => {
         >
           {course?.author}
         </Text>
-        {course?.chapter?.length ? (
-          <View
-            style={{
-              display: "flex",
-              flexDirection: "row",
-              gap: 4,
-              alignItems: "center",
-            }}
-          >
-            <Ionicons name="book" size={24} color={Colors.PRIMARY} />
-            <Text style={{ color: Colors.GRAY }}>{course?.chapter.length}</Text>
-          </View>
-        ) : null}
-        <Text>{}</Text>
+        <View
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          {course?.chapter?.length ? (
+            <View
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                gap: 4,
+                alignItems: "center",
+              }}
+            >
+              <Ionicons name="book" size={24} color={Colors.PRIMARY} />
+              <Text style={{ color: Colors.GRAY, fontFamily: "outfit-bold" }}>
+                {course?.chapter.length}
+              </Text>
+            </View>
+          ) : (
+            <View
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                gap: 4,
+                alignItems: "center",
+              }}
+            >
+              <Ionicons name="logo-youtube" size={20} color={"red"} />
+              <Text style={{ color: Colors.GRAY, fontFamily: "outfit-bold" }}>
+                Watch On Youtube
+              </Text>
+            </View>
+          )}
+
+          <Text style={{ fontFamily: "outfit-bold", color: Colors.PRIMARY }}>
+            {course.free ? "Free" : "Paid"}
+          </Text>
+        </View>
       </View>
     </View>
   );
